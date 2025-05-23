8236d0ca629b22e0c56d7356c549834c43e2a33b
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-05-23 14:28:40 +0700

    Patch: Add GitHub icon to header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
     <div
-        class="w-full flex gap-x-4 bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
+        class="items-center w-full h-25 flex gap-x-4 bg-white p-6 shadow-lg dark:bg-slate-900"
     >
         <img
             class="size-15 shrink-0 rounded-full"
-            src="./assets/profile.png"
+            src="./assets/img/profile.png"
             alt="Profile Logo"
         />
         <div>
@@ -15,10 +15,20 @@
                     >Farhan Kurnia Pratama</a
                 >
             </div>
-            <p class="text-gray-500 dark:text-gray-400">
+            <p class="text-gray-500 dark:text-gray-4ss00">
                 ✨ Creative thinker and problem solver who loves creating free
                 and open-source software (FOSS).
             </p>
+        </div>
+        <div
+            class="flex ml-auto"
+        >
+            <a href="https://github.com/farhnkrnapratma">
+                <i
+                    class="bi bi-github cursor-pointer text-4xl text-gray-400 hover:text-gray-300 shrink-0"
+                    alt="GitHub Logo"
+                />
+            </a>
         </div>
     </div>
 </template>
