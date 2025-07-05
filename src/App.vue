6598e0dd3bcaf83deb99b39fca05b4564a27b485
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-07-06 01:35:05 +0700

    Patch: Designed new UI

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,60 @@
 <template>
     <div
-        class="items-center w-full h-25 flex gap-x-4 bg-white p-6 shadow-lg dark:bg-slate-900"
+        class="w-full h-full absolute top-0 left-0 grid [grid-template-columns:15%_85%] [grid-template-rows:4%_96%] p-2 bg-base whitespace-nowrap"
     >
-        <img
-            class="size-15 shrink-0 rounded-full"
-            src="./assets/img/profile.png"
-            alt="Profile Logo"
-        />
-        <div>
-            <div
-                class="cursor-pointer text-xl font-medium text-black dark:text-white"
-            >
-                <a href="https://farhankurniapratama.my.id"
-                    >Farhan Kurnia Pratama</a
-                >
-            </div>
-            <p class="text-gray-500 dark:text-gray-4ss00">
-                ✨ Creative thinker and problem solver who loves creating free
-                and open-source software (FOSS).
-            </p>
-        </div>
         <div
-            class="flex ml-auto"
+            class="col-span-full bg-base rounded-sm grid grid-cols-2 grid-rows-1"
         >
-            <a href="https://github.com/farhnkrnapratma">
-                <i
-                    class="bi bi-github cursor-pointer text-4xl text-gray-400 hover:text-gray-300 shrink-0"
-                    alt="GitHub Logo"
-                />
-            </a>
+            <div
+                class="col-span-1 row-span-1 inline-flex items-center w-full h-full"
+            >
+                <ul class="list-none inline-flex ml-2 hlist">
+                    <li class="bi bi-layout-sidebar-inset" tabindex="0"></li>
+                    <li tabindex="0">File</li>
+                    <li tabindex="0">Edit</li>
+                    <li tabindex="0">View</li>
+                    <li tabindex="0">Sort</li>
+                </ul>
+            </div>
+            <div
+                class="col-span-1 row-span-1 inline-flex items-center justify-end w-full h-full"
+            >
+                <ul class="list-none inline-flex mr-2 hlist">
+                    <li class="bi bi-search" tabindex="0"></li>
+                    <li class="bi bi-x-lg" tabindex="0"></li>
+                </ul>
+            </div>
         </div>
+        <div class="bg-surface0 rounded-sm">
+            <ul class="list-none mlist">
+                <li tabindex="0"><i class="bi bi-clock-history"></i>Recents</li>
+                <li tabindex="0"><i class="bi bi-house"></i>Home</li>
+                <li tabindex="0"><i class="bi bi-file-text"></i>Documents</li>
+                <li tabindex="0">
+                    <i class="bi bi-file-earmark-arrow-down"></i>Downloads
+                </li>
+                <li tabindex="0">
+                    <i class="bi bi-music-note-beamed"></i>Music
+                </li>
+                <li tabindex="0"><i class="bi bi-image"></i>Pictures</li>
+                <li tabindex="0">
+                    <i class="bi bi-person-workspace"></i>Projects
+                </li>
+                <li tabindex="0"><i class="bi bi-share"></i>Public</li>
+                <li tabindex="0"><i class="bi bi-copy"></i>Templates</li>
+                <li tabindex="0"><i class="bi bi-play-btn"></i>Videos</li>
+                <hr
+                    class="rounded-md h-[2px] bg-surface1 border-none m-3 ml-6 mr-6"
+                />
+                <li tabindex="0"><i class="bi bi-trash"></i>Trash</li>
+                <hr
+                    class="rounded-md h-[2px] bg-surface1 border-none m-3 ml-6 mr-6"
+                />
+                <li tabindex="0">
+                    <i class="bi bi-globe-americas"></i>Networks
+                </li>
+            </ul>
+        </div>
+        <div class="bg-base rounded-sm"></div>
     </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [vue(), tailwindcss()],
+    plugins: [vue(), tailwindcss()],
 });
