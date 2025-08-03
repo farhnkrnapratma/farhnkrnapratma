5bd55bf707164827eee57e2230db6a1dc161893f
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2025-08-03 18:47:48 +0700

    Chore: changed framework and development tools: npm -> bun, vite -> rsbuild, biome -> oxlint, vue -> react
    Signed-off-by: Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+	return (
+		<div>
+			<p className={"font-black text-xl"}>Hello World!</p>
+		</div>
+	);
+}
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import tailwindcss from "@tailwindcss/vite";
-import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-    plugins: [vue(), tailwindcss()],
-});
