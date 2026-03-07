980f369e70c61b0e48a1afdfbcc3e4f3de751c84
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2026-03-07 15:00:29 +0700

    add new entrypoints

--- src/build.ts
+++ src/build.ts
@@ -1,6 +1,13 @@
 Bun.build({
   publicPath: "https://farhnkrnapratma.dev/",
-  entrypoints: ["./src/index.html"],
+  entrypoints: [
+    "./src/index.html",
+    "./src/pages/blog/index.html",
+    "./src/pages/contact/index.html",
+    "./src/pages/donate/index.html",
+    "./src/pages/feed/index.html",
+    "./src/pages/social/index.html",
+  ],
   outdir: "./build",
   naming: {
     asset: "[dir]/[name].[ext]",
