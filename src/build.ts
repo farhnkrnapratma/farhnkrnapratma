a6f0e91f854bfbb2d0a7561b7c40dd95d4fcf428
Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>
2026-03-23 08:39:22 +0700

    fix: missing favicon and banner
    Signed-off-by: Farhan Kurnia Pratama <farhnkrnapratma@gmail.com>

--- src/build.ts
+++ src/build.ts
@@ -1,11 +1,15 @@
 await Bun.build({
+  publicPath: "https://farhnkrnapratma.dev/",
   entrypoints: ["./src/index.html"],
   outdir: "./build",
   naming: {
-    asset: "[name].[ext]",
+    asset: "[dir]/[name].[ext]",
     entry: "[dir]/[name].[ext]",
     chunk: "[dir]/[name].[ext]",
   },
   sourcemap: "inline" as const,
   minify: true,
 });
+
+await Bun.write("./build/favicon", Bun.file("./src/assets/favicon.ico"));
+await Bun.write("./build/banner", Bun.file("./src/assets/banner.png"));
